f8fdbde2cf93c46a6995bbe1ceb89f5f36e1de76
marlonfa <marlonfa@gmail.com>
2013-02-10 10:46:44 -0200

    Enviando o TesteMarlon

---
+++ TesteMarlon.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class exe01 {
+	
+	public static void main (String args[]) {		
+		Scanner ent = new Scanner (System.in);
+		
+		System.out.println("Digite a Temperatura em graus Celsius:");
+			double gc = ent.nextDouble();
+			
+			double gf;
+			gf = (9*gc + 160)/5;
+			
+		System.out.println(gc+" graus Celsius é = "+gf+" Fahrenheit.");
+	}
+}
+
